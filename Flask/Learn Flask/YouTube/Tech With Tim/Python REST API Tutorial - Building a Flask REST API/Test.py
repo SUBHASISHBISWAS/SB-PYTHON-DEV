1db8bb3881a93c5f1bb4d8d26104f1880b4d593c
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2021-11-03 20:11:21 +0530

    Grabbing Request

--- Flask/Learn Flask/YouTube/Tech With Tim/Python REST API Tutorial - Building a Flask REST API/Test.py
+++ Flask/Learn Flask/YouTube/Tech With Tim/Python REST API Tutorial - Building a Flask REST API/Test.py
@@ -2,8 +2,12 @@ import  requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response=requests.get(BASE+"helloworld/SUBHASISH/38")
+
+response = requests.put(BASE+"video/1",{"likes":10})
 print(response.json())
 
-response=requests.post(BASE+"helloworld")
-print(response)
+#response=requests.get(BASE+"helloworld/SUBHASISH/38")
+#print(response.json())
+
+#response=requests.post(BASE+"helloworld")
+#print(response)
--- Flask/Learn Flask/YouTube/Tech With Tim/Python REST API Tutorial - Building a Flask REST API/main.py
+++ Flask/Learn Flask/YouTube/Tech With Tim/Python REST API Tutorial - Building a Flask REST API/main.py
@@ -1,18 +1,22 @@
-from flask import Flask
+from flask import Flask,request
 from flask_restful import Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
 
 
-class HelloWorld(Resource):
+class Video(Resource):
     def get(self, name, age):
         return {name:age}
 
     def post(self):
         return {"POST", "SUBHASISH"}
 
+    def put(self,video_id):
+        print(request.form['likes'])
+        return {}
 
-api.add_resource(HelloWorld, "/helloworld/<string:name>/<int:age>")
+
+api.add_resource(Video, "/video/<int:video_id>")
 if __name__ == "__main__":
     app.run(debug=True)
