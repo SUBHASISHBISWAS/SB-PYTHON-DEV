bfdbc351104e095aba99083fc1c088a396e29f90
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2021-11-03 21:14:38 +0530

    put and delete

--- Flask/Learn Flask/YouTube/Tech With Tim/Python REST API Tutorial - Building a Flask REST API/Test.py
+++ Flask/Learn Flask/YouTube/Tech With Tim/Python REST API Tutorial - Building a Flask REST API/Test.py
@@ -1,13 +1,21 @@
-import  requests
+import requests
 
 BASE = "http://127.0.0.1:5000/"
 
+data = [{"likes": 78, "name": "Joe", "views": 100000},
+        {"likes": 10000, "name": "HOW to make REST API", "views": 80000},
+        {"likes": 35, "name": "Tim", "views": 2000}]
 
-response = requests.put(BASE+"video/1",{"likes":10})
+for i in range(len(data)):
+    response=requests.put(BASE+"video/"+str(i),data[i])
+    print(response.json())
+
+input()
+response = requests.put(BASE + "video/1", {"likes": 10000, "name": "SUBHASISH", "views": 100000})
+print(response.json())
+input()
+response = requests.get(BASE + "video/1")
 print(response.json())
 
-#response=requests.get(BASE+"helloworld/SUBHASISH/38")
-#print(response.json())
-
-#response=requests.post(BASE+"helloworld")
-#print(response)
+# response=requests.post(BASE+"helloworld")
+# print(response)
--- Flask/Learn Flask/YouTube/Tech With Tim/Python REST API Tutorial - Building a Flask REST API/main.py
+++ Flask/Learn Flask/YouTube/Tech With Tim/Python REST API Tutorial - Building a Flask REST API/main.py
@@ -1,20 +1,43 @@
-from flask import Flask,request
-from flask_restful import Api, Resource
+from flask import Flask, request
+from flask_restful import Api, Resource, reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
 
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Name os the Video", required=True)
+video_put_args.add_argument("views", type=int, help="Views os the Video", required=True)
+video_put_args.add_argument("likes", type=int, help="Likes os the Video", required=True)
+
+videos = {}
+
+
+def abort_if_video_does_not_exist(video_id):
+    if video_id not in videos:
+        abort(404, message="Video id not Exist")
+
+def abort_if_video_exist(video_id):
+    if video_id  in videos:
+        abort(409, message="Video id already Exist")
 
 class Video(Resource):
-    def get(self, name, age):
-        return {name:age}
+    def get(self, video_id):
+        abort_if_video_does_not_exist(video_id)
+        return videos[video_id]
 
     def post(self):
         return {"POST", "SUBHASISH"}
 
-    def put(self,video_id):
-        print(request.form['likes'])
-        return {}
+    def put(self, video_id):
+        abort_if_video_exist(video_id)
+        args = video_put_args.parse_args()
+        videos[video_id] = args
+        return videos[video_id], 201
+
+    def delete(self,video_id):
+        abort_if_video_does_not_exist(video_id)
+        del videos[video_id]
+        return '',204
 
 
 api.add_resource(Video, "/video/<int:video_id>")
